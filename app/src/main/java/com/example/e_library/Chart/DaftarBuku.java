8134bef96ae8c7f3b2c71940240ed15e911db11a
Rqwannn <mohwannn1@gmail.com>
2022-04-30 12:02:24 +0700

    revisi interface konfirmasi

--- app/src/main/java/com/example/e_library/Chart/DaftarBuku.java
+++ app/src/main/java/com/example/e_library/Chart/DaftarBuku.java
@@ -1,13 +1,24 @@
 package com.example.e_library.Chart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.e_library.R;
 
 public class DaftarBuku extends AppCompatActivity {
+
+    RecyclerView rvData;
+    RecyclerView.LayoutManager rlData;
+    RecyclerView.Adapter raData;
+    SwipeRefreshLayout SWL;
+    ProgressBar PBData;
+    SharedPreferences SessionStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
