aa910ef324906bb12cab7bcf45ae613c1134fb7f
Rqwannn <mohwannn1@gmail.com>
2022-05-06 21:51:25 +0700

    change book url

--- app/src/main/java/com/example/e_library/Model/RetroServer.java
+++ app/src/main/java/com/example/e_library/Model/RetroServer.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetroServer {
     public static final String baseURL = "http://192.168.1.13/elibrary-app/public/api/";
-    public static final String imgBukuURL = "http://192.168.1.13/elibrary-app/public/assets/img/books/";
+    public static final String imgBukuURL = "http://192.168.1.13/elibrary-app/public/";
     public static Retrofit Server;
 
     public static Retrofit KonekServer(){
